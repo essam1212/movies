3b9bb5ce64a29cd5455a78b211a32ebcfb2f79bc
essam1212 <essam5ali2000@gmail.com>
2023-01-23 23:53:48 +0200

    comit 2

--- src/componant/About/About.jsx
+++ src/componant/About/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import about from '../../../public/img/unnamed.jpg'
+import about from '../../img/unnamed.jpg'
 export default function About() {
   return (
     <div className=" container">
