2b8f05f9aa122bee6c6f8f021363dfbd143a562a
essam1212 <essam5ali2000@gmail.com>
2023-01-27 15:59:35 +0200

    commit 1

--- src/componant/Navpar/Navpar.jsx
+++ src/componant/Navpar/Navpar.jsx
@@ -2,42 +2,36 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Navpar(props) {
-  const [expand, updateExpanded] = useState(false);
 
   return <>
     
    
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark  bg-transparent mb-5 pb-5">
-        <Link className="navbar-brand fw-bolder" to="home" onClick={() => updateExpanded(false)}>Noxe</Link>
+        <Link className="navbar-brand fw-bolder" to="/home" >Noxe</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto text-center mt-2 mt-lg-0">
                 <li className="nav-item">
-                  <Link className="nav-link" to="home"
-                   onClick={() => updateExpanded(false)}
+                  <Link className="nav-link" to="/home"
                   >Home</Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="about"
-                   onClick={() => updateExpanded(false)}
+                  <Link className="nav-link" to="/about"
                   >about</Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="movis"
-                   onClick={() => updateExpanded(false)}
+                  <Link className="nav-link" to="/movis"
                   >movis</Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="Tv"
-                   onClick={() => updateExpanded(false)}
+                  <Link className="nav-link" to="/tv"
                   >Tv</Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="people"
-                   onClick={() => updateExpanded(false)}
+                  <Link className="nav-link" to="/people"
                   >People</Link>
                 </li>
 
