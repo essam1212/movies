4c9c7374a2f67472d8e8d2fd23ba01f7d0efe0ac
essam1212 <essam5ali2000@gmail.com>
2023-02-03 16:30:26 +0200

    commit 1

--- src/componant/Navpar/Navpar.jsx
+++ src/componant/Navpar/Navpar.jsx
@@ -13,7 +13,7 @@ export default function Navebar(props) {
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <ul className="navbar-nav text-center me-auto mb-2 mb-lg-0">
                             
                                 
                                     <li className="nav-item">
